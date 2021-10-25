281c7015d0786f658d7f39d9f84962088652e231
hend <hendhossamelden@gmail.com>
2021-10-25 22:33:56 +0200

    intial project added embty homepage screen

---
+++ lib/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class homepage extends StatelessWidget
+{
+  static const String routeName='home_page';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container()
+    );
+  }
+}
